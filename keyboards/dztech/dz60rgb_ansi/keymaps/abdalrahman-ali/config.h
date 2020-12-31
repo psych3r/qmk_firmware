66338d0e901cf59f857b47fff14ce6d8741b7c70
Psycher <abdalrahman.mursi@gmail.com>
2020-12-31 21:28:29 +0200

    adding a new keymap to dz60rgb_ansi_v2

---
+++ keyboards/dztech/dz60rgb_ansi/keymaps/abdalrahman-ali/config.h
@@ -0,0 +1,14 @@
+
+// tapping timeout in ms, for TAPPING_TERM_PER_KEY, refer to the docs
+#define TAPPING_TERM 200
+
+// enable if fast tap modding inserts two tapped keys, refer to docs
+//#define PERMISSIVE_HOLD
+
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// tab moded key then hold it 
+// TAPPING_FORCE_HOLD will break anything that uses tapping toggles (Such as the TT layer keycode, and the One Shot Tap Toggle).
+// there's a per key variant.
+// #define TAPPING_FORCE_HOLD
+// consider enabling this per key for home row mods
