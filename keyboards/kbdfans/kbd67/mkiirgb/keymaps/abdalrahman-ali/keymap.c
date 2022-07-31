ded58fe98744851531736d0962cfd4d09ec54b81
Abdalrahman Mursi <amursi@mentor.com>
2022-07-31 13:46:55 +0200

    gui scln home row mod for the blade

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/abdalrahman-ali/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/abdalrahman-ali/keymap.c
@@ -11,6 +11,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t* record)
         case CTLD: return TAPPING_TERM + 50; break;
         case SFTF: return TAPPING_TERM - 10; break;
         case SFTJ: return TAPPING_TERM - 10; break;
+        case GUIN: return TAPPING_TERM + 50; break;
         default:   return TAPPING_TERM;
     }
 }
@@ -20,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     [_base] = LAYOUT_65_ansi_blocker(
         KC_GESC, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
         KC_TAB,  MODS,    HJKL,    KC_E,   KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
-        TMUX,    GUIA,    ALTS,    CTLD,   SFTF,   KC_G,   KC_H,    SFTJ,    CTLK,    ALTL,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_END,
+        TMUX,    GUIA,    ALTS,    CTLD,   SFTF,   KC_G,   KC_H,    SFTJ,    CTLK,    ALTL,    GUIN,    KC_QUOT,          KC_ENT,  KC_END,
         SCAP,    KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   TT(_hjkl),
         KC_LCTL, KC_LGUI, KC_LALT,                         KC_SPC,                  TT(_vrgb), KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
     ),
