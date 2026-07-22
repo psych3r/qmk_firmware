2f6bffd6994599eb0a36b66c29afde44e81bf782
Abdalrahman Mursi <abdalrahman.mursi@siemens.com>
2026-07-22 19:12:18 +0300

    fix the backspace key on new, enable os detection (doesnt work), return false after os_terminate

--- users/psycher/psycher.c
+++ users/psycher/psycher.c
@@ -46,7 +46,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record)
                         break;
                 }
             }
-            // return false;
+            return false;
             break;
         default:
             return true; // Process all other keycodes normally
